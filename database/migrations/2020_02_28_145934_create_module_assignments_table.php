489c72827b0b8cbe17a3d1ebba38f7924f72626f
“Lars <versteeg0@outlook.com>
2020-02-28 16:26:57 +0100

    Module & assignment database migrations.

---
+++ database/migrations/2020_02_28_145934_create_module_assignments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateModuleAssignmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assignment_module', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('module_id');
+            $table->unsignedBigInteger('assignment_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('module_assignments');
+    }
+}
